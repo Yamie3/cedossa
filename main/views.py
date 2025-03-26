3d6427424c82aa0138f242094accfcd21ebd9998
Yamie3 <yamkamwendo@outlook.com>
2025-03-26 20:53:43 +0200

    added forms on the contact us and donate pages

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 def home(request):
     return render(request, 'index.html')  # Render homepage template
@@ -11,3 +12,14 @@ def contact(request):
 
 def donate(request):
     return render(request, 'donate.html')  # Render the Donate page
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form (save to database, send email, etc.)
+            return render(request, 'contact_success.html')  # Success page after form submission
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})

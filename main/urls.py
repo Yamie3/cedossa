6de725f94d3256963f9a2e78f09999ef9f0c186f
Yamie3 <yamkamwendo@outlook.com>
2025-03-26 20:35:26 +0200

    Added Contact and Donate Page

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Homepage
-        path('about/', views.about, name='about'),  # About page
+    path('about/', views.about, name='about'),  # About page
+    path('contact/', views.contact, name='contact'),  # Contact page
+    path('donate/', views.donate, name='donate'),  # Donate page
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')  # Render the custom About page
+
+def contact(request):
+    return render(request, 'contact.html')  # Render the Contact page
+
+def donate(request):
+    return render(request, 'donate.html')  # Render the Donate page

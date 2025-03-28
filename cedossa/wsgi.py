fede5b5afe9c0d801abd5c9655f6d17b7a23f2d1
Yamie3 <yamkamwendo@outlook.com>
2025-03-28 19:35:36 +0200

    Added urls for Renderr deployment

--- cedossa/wsgi.py
+++ cedossa/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cedossa')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cedossa.settings')  # Make sure 'cedossa' is your project name
 
 application = get_wsgi_application()
-
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Homepage
     path('about/', views.about, name='about'),  # About page
     path('contact/', views.contact, name='contact'),  # Contact page
     path('donate/', views.donate, name='donate'),  # Donate page
+    path('', include('cedossa.urls')),
 ]

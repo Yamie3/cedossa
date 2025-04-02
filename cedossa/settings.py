16311984c17e4000c7c3f5e69cbbc51d54f2527c
Yamie3 <yamkamwendo@outlook.com>
2025-04-02 21:34:27 +0200

    refreshed code removed changed the navigation bar

--- cedossa/settings.py
+++ cedossa/settings.py
@@ -104,9 +104,9 @@ LANGUAGES = [
 
 # Static & Media Files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.conf.urls import handler404
 handler404 = 'main.views.page_not_found'
+from django.conf import settings  # Add this
+from django.conf.urls.static import static  # Add this
 
 
 app_name = 'main'  # Namespace for URLs
@@ -13,3 +15,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('contact/success/', views.contact_success, name='contact_success'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

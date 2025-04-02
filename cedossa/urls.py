e86281e6d24360a45434a675e76b0ead1b687b0c
Yamie3 <yamkamwendo@outlook.com>
2025-04-02 20:27:49 +0200

    refreshed code removed static folder

--- cedossa/urls.py
+++ cedossa/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  # Add this
+from django.conf.urls.static import static  # Add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),  # All URLs delegated to main app
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
